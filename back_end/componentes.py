6b616feabfdab14972944bde37de36395a92c31f
Ruggerini <carlosruggiero6@gmail.com>
2023-11-22 18:23:43 -0600

    frontend
    frontend

--- back_end/componentes.py
+++ back_end/componentes.py
@@ -3,19 +3,19 @@ class Componentes:
     def __init__(self, db):
         self.db = db
 
-    def insert(self, id_Componente,Nombre_Componente,Vehiculo_idVehiculo):
+    def insert(self, id_Componentes,Nombre_Componente,Vehiculo_idVehiculo):
         sql = "INSERT INTO componentes VALUES (%s,%s,%s)"
-        self.db.cursor.execute(sql, (id_Componente,Nombre_Componente,Vehiculo_idVehiculo))
+        self.db.cursor.execute(sql, (id_Componentes,Nombre_Componente,Vehiculo_idVehiculo))
         self.db.commit()
 
-    def update(self, id_Componente, nombre_componente):
-        sql = "UPDATE componentes SET nombre_componente  = %s WHERE id_Componente = %s"
+    def update(self, id_Componentes, Nombre_Componente):
+        sql = "UPDATE componentes SET nombre_componente  = %s WHERE id_Componentes = %s"
         self.db.cursor.execute(sql, (nombre_componente, id_Componente))
         self.db.commit()
 
-    def delete(self, id_Componente):
-        sql = "DELETE FROM componentes WHERE id_Componente = %s"
-        self.db.cursor.execute(sql, (id_Componente,))
+    def delete(self, id_Componentes):
+        sql = "DELETE FROM componentes WHERE id_Componentes = %s"
+        self.db.cursor.execute(sql, (id_Componentes,))
         self.db.commit()
 
     def get_all_componentes(self):
--- back_end/marca.py
+++ back_end/marca.py
@@ -3,15 +3,15 @@ class Marca:
     def __init__(self, db):
         self.db = db
 
-    def insert(self,nombre_marca,idMarca):
+    def insert(self,idMarca,Nombre_Marca):
         sql = "INSERT INTO marca VALUES (%s,%s)"
-        self.db.cursor.execute(sql,(idMarca,nombre_marca))
+        self.db.cursor.execute(sql,(idMarca,Nombre_Marca))
         self.db.commit()
           
 
-    def update(self, Nombre_Marca,idMarca):
+    def update(self, idMarca,Nombre_Marca):
          sql = "UPDATE marca SET Nombre_Marca = %s WHERE idMarca = %s"
-         self.db.cursor.execute(sql, (Nombre_Marca,idMarca))
+         self.db.cursor.execute(sql, (idMarca,Nombre_Marca))
          self.db.commit()
 
     def delete(self, idMarca):
